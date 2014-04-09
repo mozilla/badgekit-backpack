798eb5e1a6a2eddedca310db5e033bfa3e01f251
Brian J Brennan <brianloveswords@gmail.com>
2014-04-09 14:29:04 -0400

    Test cascading delete.

--- models/earners.js
+++ models/earners.js
@@ -2,28 +2,28 @@ const db = require('../lib/db')
 const Earners = db.table('earners', {
   fields: [ 'id', 'createdOn' ],
   relationships: {
-    badges: {
-      type: 'hasMany',
-      local: 'id',
-      foreign: { table: 'earnerBadges', key: 'earnerId' },
-      optional: true,
-    },
-    extra: {
+    // badges: {
+    //   type: 'hasMany',
+    //   local: 'id',
+    //   foreign: { table: 'earnerBadges', key: 'earnerId' },
+    //   optional: true,
+    // },
+    _metadata: {
       type: 'hasMany',
       local: 'id',
       foreign: { table: 'earnerData', key: 'earnerId' },
       optional: true,
     },
   },
-})
-const EarnerData = db.table('earnerData', {
-  fields: [ 'id', 'earnerId', 'key', 'value' ],
-  relationships: {
-    earner: {
-      type: 'hasOne',
-      local: 'earnerId',
-      foreign: { table: 'earners', key: 'id' },
+  methods: {
+    metadata: function () {
+      const data = this._metadata || []
+      return data.reduce(function (acc, datum) {
+        acc[datum.key] = datum.value
+        return acc
+      }, {})
     }
   }
 })
+
 module.exports = Earners
--- test/models/earners.test.js
+++ test/models/earners.test.js
@@ -1,5 +1,6 @@
 const test = require('tap').test
 const prepare = require('./')
+const EarnerData = require('../../models/earner-data')
 const Earners = require('../../models/earners')
 
 prepare().then(function (db) {
@@ -12,21 +13,57 @@ prepare().then(function (db) {
       })
 
       .then(function(earner) {
-        t.same(earner.id, data.id, 'should have right id')
-        t.ok(earner.createdOn, 'should have createdOn date')
+        t.same(earner.id, data.id, 'has right id')
+        t.ok(earner.createdOn, 'has createdOn date')
         return Earners.del(data)
       })
 
       .then(function(result) {
-        t.same(result.affectedRows, 1, 'should delete 1 row')
+        t.same(result.affectedRows, 1, 'deletes 1 row')
         return Earners.getOne(data)
       })
 
       .then(function(earner) {
-        t.notOk(earner, 'should not find an earner')
+        t.notOk(earner, 'does not find an earner')
         t.end()
       })
   })
+
+  test('Retrieving associated earner data', function (t) {
+    const expect = {
+      sharks: 'keep moving',
+      bears: ', minus the',
+      complex: '{"array":[1,2,3]}'
+    }
+    const query = {id: 'test-user'}
+    const options = {relationships: true}
+
+    Earners.getOne(query, options)
+      .then(function(earner) {
+        t.same(earner.metadata(), expect, 'gets correct user metadata')
+        t.end()
+      })
+  })
+
+  test('Earner data should get deleted when earner is deleted', function (t) {
+    const earnerId = 'delete-me'
+    EarnerData.get({earnerId: earnerId})
+      .then(function(data) {
+        t.same(data.length, 1, 'gets one result')
+        t.same(data[0].key, 'fool me once', 'gets the right key')
+        return Earners.del({id: earnerId})
+      })
+
+      .then(function(result) {
+        return EarnerData.get({earnerId: earnerId})
+      })
+
+      .then(function(data) {
+        t.same(data.length, 0, 'associated data gets deleted')
+        t.end()
+      })
+  })
+
 
   test('--close--', function (t) {
     db.close(), t.end()
